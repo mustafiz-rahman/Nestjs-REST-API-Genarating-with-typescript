a3939b16d0aaa35914d03d31ab6bf27ec3a10e39
mustafiz-rahman <mustafiz.iub@gmail.com>
2021-11-08 01:24:34 +0600

    validation with dtos

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post ,Body, Param} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { createMessageDto } from './dtos/create-message.dto';
+ 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get()
+  listMessages() {}
+  @Post()
+  createMessage(@Body() body:createMessageDto) {
+      console.log(body);
+  }
+  @Get('/:id')
+  getMessages(@Param('id') id:string) {
+      console.log(id);
+  }
+}
